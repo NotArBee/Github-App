8bdc08dd0bb2e9d74460e958a4ad406d3028e246
AR <ardevelop01@gmail.com>
2024-03-28 12:08:47 +0800

    feat: function searchUsers ditambahkan

--- app/src/main/java/com/ardev/githubapp/ui/MainViewModel.kt
+++ app/src/main/java/com/ardev/githubapp/ui/MainViewModel.kt
@@ -51,26 +51,26 @@ class MainViewModel : ViewModel() {
         })
     }
 
-//    private fun showUsersSearch() {
-//        _isLoading.value = false
-//        val client = ApiConfig.getApiService().getListUsers(search)
-//        client.enqueue(object : Callback<GithubResponse> {
-//            override fun onResponse(
-//                call: Call<GithubResponse>,
-//                response: Response<GithubResponse>
-//            ) {
-//                _isLoading.value = false
-//                if (response.isSuccessful) {
-//                    _itemsItem.value = response.body()?.items
-//                } else {
-//                    Log.e(TAG, "onFailure : ${response.message()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
-//                _isLoading.value = false
-//                Log.e(TAG, "onFailure : ${t.message.toString()}")
-//            }
-//        })
-//    }
+    fun searchUsers(keyword: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getListUsers(keyword)
+        client.enqueue(object : Callback<GithubResponse> {
+            override fun onResponse(
+                call: Call<GithubResponse>,
+                response: Response<GithubResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _itemsItem.value = response.body()?.items
+                } else {
+                    Log.e(TAG, "onFailure : ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure : ${t.message.toString()}")
+            }
+        })
+    }
 }
